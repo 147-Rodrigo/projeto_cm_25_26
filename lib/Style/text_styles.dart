24d47f8a567484cea76f28db9dc8fc1dfa746e00
147-Rodrigo <202300147@estudantes.ips.pt>
2026-05-25 14:41:06 +0100

    InfoPage

---
+++ lib/Style/text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle welcomeTitle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle cardTitle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle cardText = TextStyle(
+    fontSize: 14,
+    color: Colors.black,
+  );
+}
